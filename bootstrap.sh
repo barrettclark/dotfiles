070422cf0f62c103035cdbc760c98bd10bca2eb3
Barrett Clark <barrett@websitesbybarrett.com>
2015-10-26 08:52:25 -0500

    Linux!!!

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,25 +9,25 @@ function doIt() {
   # This does NOT run ./osx or ./brew.sh
   rm -rf ~/.vim
 
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" --exclude "fonts/" \
+  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+    --exclude "README.md" --exclude "LICENSE-MIT.txt" --exclude "fonts/" \
     --exclude "init/" --exclude ".osx" --exclude "brew.sh" \
-    --exclude "tmux_setup.sh" -avh --no-perms . ~;
+    --exclude "tmux_setup.sh" --exclude ".linux" -avh --no-perms . ~;
   rsync --exclude ".DS_Store" -av --no-perms fonts/ ~/Library/Fonts/
 
   git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/Vundle.vim
   vim +PluginInstall +qall
 
-	source ~/.bash_profile;
+  source ~/.bash_profile;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
+  doIt;
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
+  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+  echo "";
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    doIt;
+  fi;
 fi;
 unset doIt;
