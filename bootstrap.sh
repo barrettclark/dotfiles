1c342ec8de0cbdddf8848b3bfaef5c9abe06f833
Barrett Clark <barrett@websitesbybarrett.com>
2016-07-22 09:25:42 -0500

    More flexible bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,29 +4,47 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin master;
 
-function doIt() {
+function copyDotFiles() {
   # This copies (rsynchs) everything not specifically excluded to $HOME
-  # This does NOT run ./osx or ./brew.sh
-  rm -rf ~/.vim
-
   rsync -avh --no-perms --progress the_dot_files/ $HOME
   rsync -avh --no-perms --progress bin/ $HOME/bin
   rsync --exclude ".DS_Store" -av --no-perms --progress fonts/ ~/Library/Fonts/
   # cp init/sadserver_tweets.dat /usr/local/share/games/fortunes
-
-  git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/Vundle.vim
-  vim +PluginInstall +qall
-
   source ~/.bash_profile;
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-  doIt;
-else
-  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-  echo "";
-  if [[ $REPLY =~ ^[Yy]$ ]]; then
-    doIt;
-  fi;
+function setupVim() {
+  rm -rf ~/.vim
+  git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/Vundle.vim
+  vim +PluginInstall +qall
+}
+
+function setupTmux() {
+  rm -rf ~/.tmux/plugins
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  tmux source ~/.tmux.conf
+}
+
+read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+echo "";
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  copyDotFiles;
+  while getopts ":ol" opt; do
+    case $opt in
+      o)
+        echo "Bootstrap OSX"
+        bootstrap_osx.sh
+        ;;
+      l)
+        echo "Bootstrap Linux"
+        bootstrap_linux.sh
+        ;;
+    esac
+  done
+  setupVim;
+  setupTmux;
 fi;
-unset doIt;
+
+unset copyDotFiles;
+unset setupVim;
+unset setupTmux;
