1d7632e5a55f692b0d0e2b13ec75d92b226b6cd6
Barrett Clark <code@cookingco.de>
2017-04-10 20:01:00 -0500

    Update openssh

--- brew.sh
+++ brew.sh
@@ -44,7 +44,7 @@ brew install wget --with-iri
 # Install more recent versions of some OS X tools.
 brew install vim --override-system-vi
 brew install homebrew/dupes/grep
-brew install homebrew/dupes/openssh
+brew install openssh
 
 # Install other useful things.
 brew install ack
