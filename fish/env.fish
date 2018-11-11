16c77cd97cd4857bd433d868a03904f628edac17
Barrett Clark <code@cookingco.de>
2018-11-11 10:52:53 -0600

    Add homebrew item to path

--- fish/env.fish
+++ fish/env.fish
@@ -19,6 +19,7 @@ append-to-path ~/Library/Python/2.7/bin
 prepend-to-path /usr/local/sbin
 set -gx PG_CONFIG /Applications/Postgres.app/Contents/Versions/9.6/bin
 append-to-path $PG_CONFIG
+prepend-to-path /usr/local/opt/libarchive/bin
 # }}}
 
 set JDK jdk1.8.0_112.jdk
