bddde4ea43be03b443876c5773267c9f1685ea55
Barrett Clark <barrett.clark@hashicorp.com>
2026-07-10 11:07:45 -0500

    Update tmux now playing to work with plexamp

--- bin/tmux-current-track.sh
+++ bin/tmux-current-track.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # Inspired by https://gist.github.com/fholgado/1982770
 
-APPLE_MUSIC=$( osascript <<EOF
+APPLE_MUSIC=$(osascript <<EOF
   if application "Music" is running then
     tell application "Music"
       if player state is playing then
-        return "♫ " & (get artist of current track) & " – " & (get name of current track)
+        return " " & (get artist of current track) & " – " & (get name of current track)
       else
         return ""
       end if
@@ -15,11 +15,11 @@ APPLE_MUSIC=$( osascript <<EOF
 EOF
 )
 
-SPOTIFY_MUSIC=$( osascript <<EOF
+SPOTIFY_MUSIC=$(osascript <<EOF
   if application "Spotify" is running then
     tell application "Spotify"
       if player state is playing then
-        return "♫ " & (get artist of current track) & " – " & (get name of current track)
+        return "🎧 " & (get artist of current track) & " – " & (get name of current track)
       else
         return ""
       end if
@@ -29,14 +29,20 @@ SPOTIFY_MUSIC=$( osascript <<EOF
 EOF
 )
 
-source ~/.tautulli
-PLEX_MUSIC=$(curl --silent "$TAUTULLI_URL" | jq -r ".response.data.sessions[] | select(.username==\"$TAUTULLI_USER\" and .media_type==\"track\" and .state==\"playing\") | .grandparent_title + \" - \" + .title")
-
-# echo $APPLE_MUSIC, $SPOTIFY_MUSIC, $PLEX_MUSIC
-if [ ! -z "$APPLE_MUSIC" ]; then
-  echo $APPLE_MUSIC
-elif [ ! -z "$SPOTIFY_MUSIC" ]; then
-  echo $SPOTIFY_MUSIC
-elif [ ! -z "$PLEX_MUSIC" ]; then
-  echo $PLEX_MUSIC
+if [ -n "$APPLE_MUSIC" ]; then
+  echo "$APPLE_MUSIC"
+elif [ -n "$SPOTIFY_MUSIC" ]; then
+  echo "$SPOTIFY_MUSIC"
+elif [ -f "$HOME/.plex" ] && pgrep -ix "Plexamp" &>/dev/null; then
+  source "$HOME/.plex"
+  PLEX_MUSIC=$(curl --silent --max-time 2 \
+    -H "Accept: application/json" \
+    "$PLEX_URL/status/sessions?X-Plex-Token=$PLEX_TOKEN" \
+    | jq -r '
+      .MediaContainer.Metadata[]?
+      | select(.type == "track")
+      | select(.Player.state == "playing")
+      | "▶ " + .grandparentTitle + " – " + .title
+    ' 2>/dev/null | head -1)
+  [ -n "$PLEX_MUSIC" ] && echo "$PLEX_MUSIC"
 fi
